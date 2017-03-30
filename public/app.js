96c519d948c0259162cd25bdbd4937f4a482d8a9
John McCollum <johnvmccollum@gmail.com>
2017-03-30 13:59:08 +0100

    Get all artist details rather than the first

--- public/app.js
+++ public/app.js
@@ -18,7 +18,7 @@ var processResults = function(){
 
   var albumObjects = myAlbums.map(function(item){
     var albumName = item.name;
-    var artist = item.artists[0].name;
+    var artist = item.artists.map(function(artist){ return artist.name }).join(" / ");
     var albumCover = item.images[0].url;
 
     return new Album(albumName, artist, albumCover);
